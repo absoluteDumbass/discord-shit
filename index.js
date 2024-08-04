a0cdebf9f45319e97d91e87dd765d06e1e97309a
a-newusername <shenengong@gmail.com>
2024-08-04 19:03:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const passport = require('passport');
+const session = require('express-session');
+const Strategy = require('passport-discord').Strategy;
+const mee6 = require("mee6-levels-api");
+const path = require('path');
+const { getColorFromURL } = require("color-thief-node");
+const sql = require('connect-sqlite3')(session);
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Configure Passport
+passport.use(new Strategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.REDIRECT_URI,
+    scope: ['identify']
+},
+function(accessToken, refreshToken, profile, done) {
+    // In a real application, you might store the profile in a database
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+const sessionMiddleware = session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new sql()
+});
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.get('/', passport.authenticate('discord'));
+
+app.get('/callback',
+    passport.authenticate('discord', { failureRedirect: '/' }),
+    (req, res) => {
+        // Successful authentication
+        res.redirect('/profile');
+    }
+);
+
+app.get('/profile', (req, res) => {
+    //if (!req.isAuthenticated() || !req.user) return res.redirect('/login');
+    if (typeof req.user == "undefined") res.redirect('/');
+	mee6.getUserXp("522561390330904585", req.user.id).then(user => {
+        getColorFromURL(user.avatarUrl)
+            .then(color => {
+                console.log(`Primary of ${user.id} is ${color.join(',')}`);
+                // Store the dominant color in your server or database
+                colors[user.id] = `rgb(${color.join(", ")})`;
+                user.pp = user.level;
+                userList[req.user.id] = user;
+                res.sendFile(path.join(__dirname, 'public', 'index.html'));
+            })
+            .catch(err => {
+                console.error('Error:', err);
+            });
+	})
+});
+
+app.get('/get/:file', (req, res) => {
+    // i kinda overcomplicted this
+    switch (req.params.file) {
+        case "script.js": break;
+        case "style.css": break;
+        case "marioFont.ttf": break;
+        default: 
+            res.send("fuck you");
+            console.log(`[WARNING] gosh damn someone tryna grab something called ${req.params.file}`);
+            return;
+    }
+    res.sendFile(path.join(__dirname, 'public', req.params.file));
+});
+
+app.get('/developer.js', (req, res) => {
+    if (userList[req.user.id].id != "930064591961079849")
+    res.sendFile(path.join(__dirname, 'public', 'script.js'));
+})
+
+const userList = {};
+const colors = {
+    "0": "#116600"
+};
+let grid = [];
+
+for (let x = 0; x < 20; x++) {
+    grid.push([]);
+    for (let y = 0; y < 20; y++) {
+        grid[x].push({
+            ownerID: "0"
+        })
+    }
+}
+
+// i have NO idea what this chunk does but its essential
+const wrap = (middleware) => (socket, next) =>
+    middleware(socket.request, {}, next);
+io.use(wrap(sessionMiddleware));
+io.use(wrap(passport.initialize()));
+io.use(wrap(passport.session()));
+io.use((socket, next) => {
+    if (socket.request.user) {
+        next();
+    } else {
+        next(new Error("unauthorized"));
+    }
+});
+
+io.on('connection', (socket) => {
+    const user = userList[socket.request.user.id];
+
+    socket.on('requestUserData', () => {
+        if (typeof user == "undefined") {
+            console.log("someone was bugged idk who")
+            socket.emit("loginAgain");
+            return;
+        }
+        console.log(`[JOIN] ${user.username} joined the party!`);
+        socket.emit('userData', {user, colors, grid});
+    });
+
+    socket.on('annex', (selected) => {
+        if (selected.length*2 > user.pp) return;
+        selected.forEach((index) => {
+            const sx = Math.floor(index/20);
+            const sy = index%20;
+
+            if (grid[sx][sy].ownerID == user.id) return;
+            grid[sx][sy].ownerID = user.id;
+            user.pp -= 2;
+        })
+        console.log(`[ANNEX] ${user.username} annexed ${selected.length} provinces`);
+        io.emit('userData', {user, colors, grid});
+    })
+
+    socket.on('disconnect', () => {
+        console.log('[LEAVE] user disconnected :(');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
---
+++ public/script.js
@@ -0,0 +1,134 @@
+const socket = io();
+let loaded = false;
+let user = {};
+const UIdiv = document.getElementById('ui');
+let grid = [];
+let selected = [];
+let color = {}
+
+// the UI im talking about is in the bottom left side of the screen
+const UImode = {
+    empty: "",
+    conquer: `<p class="fat">CONQUEST</p>
+    <p>each provinces CONQUERED need 2 political power!</p>
+    <button id="annex">ANNEX @hi this is a nice easter egg, please no hacking thanks@ provinces</button>`,
+    needpp: "<p>FAILED! you need @1@ more POLITICAL POWER!</p>",
+    annexed: "<p>you successfully ANNEXED @1@ provinces!</p>",
+    notyours: "<p>unfortunately, you don't own all of the selected provinces.</p>"
+}
+UIdiv.addEventListener('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    UIset("empty", []);
+    selected = [];
+    return;
+  }
+
+  // button interections, event.target.id is the id of the button clicked
+  switch (event.target.id) {
+    case "annex":
+        if (selected.length == 0) return;
+        if (selected.length*2 <= user.pp) {
+            UIset("annexed", [selected.length]);
+            socket.emit("annex", selected);
+            selected = [];
+        } else {
+            UIset("needpp", [selected.length*2-user.pp]);
+        }
+        break;
+    default:
+        console.log("Try changing the id in the switch statement too smh");
+        break;
+  }
+})
+
+socket.on('connect', () => {
+    socket.emit('requestUserData');
+    console.log("Connected!")
+});
+
+socket.on("loginAgain", () => {
+    window.location.href = "/";
+})
+
+socket.on('userData', (syncData) => {
+    console.log(syncData);
+    user = syncData.user;
+    color = syncData.colors;
+    grid = syncData.grid;
+
+    if (user.length < 1) {
+        window.location.href = "/";
+        return;
+    }
+
+    let rank = "the FORBIDDEN white role";
+    const ranks = ["Grasshopper", "Butterfly", "Chicken", "Wolf", "Crocodile", "Bear", "Plague Doctor", "Druid", // low ranks 0-39
+        "Dragon", "UFO", "Demon", "Crabzilla", // mid ranks 40-59
+        "Necromancer", "Mage", "rat king", "god finger", "robo santa", "bowling ball", "tornado", "grey goo", "super pumpkin"]; // high ranks 60+
+    const rankId = Math.floor(user.level/5);
+    
+    document.getElementById('avatar').src = user.avatarUrl;
+    document.getElementById('greeting').innerText = `hello, ${user.username}!`;
+    //document.getElementById('discord-id').innerText = `Your Discord ID: ${user.id}`;
+    document.getElementById('discord-level').innerHTML = `your LEVEL is at a fascinating number ${user.level}!`;
+    document.getElementById('discord-rank').innerHTML = `you have the rank of ${ranks[rankId].toUpperCase()}`;
+    document.getElementById('political-power').innerText = `you currently have ${user.pp} POLITICAL POWER!`;
+    loaded = true;
+});
+
+function setup() {
+    let canvas = createCanvas(windowHeight, windowHeight);
+    canvas.parent('canvas');
+    console.log("Created Canvas");
+}
+function windowResized() {
+    //let size = min(windowWidth, windowHeight);
+    resizeCanvas(windowHeight, windowHeight);
+}
+function draw() {
+    if (!loaded) return;
+    background("#666666");
+    fill(255)
+    text("does a chicken have toes?", windowHeight/2, windowHeight/2, 30);
+    let s = windowHeight/20;
+    for (let x = 0; x < 20; x++) {
+        for (let y = 0; y < 20; y++) {
+            fill(color[grid[x][y].ownerID]);
+            if (selected.includes((x*20+y))) {
+                fill(255);
+            }
+            square(x*s, y*s, s);
+        }
+    }
+}
+
+function mouseClicked() {
+    if (mouseX<0) return;
+    const clickedX = Math.floor(mouseX/windowHeight*20);
+    const clickedY = Math.floor(mouseY/windowHeight*20);
+    const index = clickedX*20+clickedY;
+
+    const found = selected.indexOf(index);
+    if (found > -1) {
+        selected.splice(found, 1);
+    } else {
+        selected.push(index);
+    }
+
+    UIset("conquer", [`${selected.length}/${Math.floor(user.pp/2)}`]);
+}
+
+function UIset(mode, fillIn) {
+    let base = UImode[mode].split("@");
+
+    if (base.length == 1) {
+        UIdiv.innerHTML = UImode[mode];
+    }
+
+    for (let i = 0; i < fillIn.length; i++) {
+        base[1+i*2] = fillIn[i];
+    }
+
+    UIdiv.innerHTML = base.join("");
+}
